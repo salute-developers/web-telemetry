d656fa75ead25c9a68ba40b73886a016e7e0146b
Paul TGnkin <paultagankin@icloud.com>
2025-02-12 20:51:52 +0300

    Refactored webtelemetry version

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
+import packageJson from './package.json';
 
 export default defineConfig({
     test: {
         include: ['src/**/*.spec.ts'],
         globals: true,
         environment: 'jsdom',
+    },
+    define: {
+        __VERSION__: JSON.stringify(packageJson.version),
     },
 });
