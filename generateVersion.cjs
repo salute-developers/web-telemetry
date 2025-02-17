909fced4f518c122e49614816de91c4bc09a63b8
Paul TGnkin <paultagankin@icloud.com>
2025-02-17 10:45:45 +0300

    added a script for a file

---
+++ generateVersion.cjs
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+
+const packageVersion = fs.readFileSync('./package.json', 'utf8')
+
+const packageInfo = JSON.parse(packageVersion)
+
+const context = `// Этот файл сгенерирован автоматически
+export const version = '${packageInfo.version}'`;
+
+fs.writeFileSync(path.resolve(__dirname, './src/version.ts'), context)
