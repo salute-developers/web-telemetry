20ca7255006945827ab87fa5b6e0558b0beeaaf2
Sergey Orlov <orlovmail@gmail.com>
2025-02-17 23:33:36 +0300

    foo

--- generateVersion.cjs
+++ generateVersion.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageVersion = fs.readFileSync('./version.txt', 'utf8');
+const packageVersion = fs.readFileSync('./version.txt', 'utf8').trim();
 
 console.log(packageVersion)
 
