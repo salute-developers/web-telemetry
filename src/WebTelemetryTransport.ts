3d25e20f21dac2a7e87d1b0a9f254bdde63ff41e
pitagankin <paultagankin@icloud.com>
2025-03-18 10:17:34 +0300

    Adding transport with headers

--- src/WebTelemetryTransport.ts
+++ src/WebTelemetryTransport.ts
@@ -18,6 +18,29 @@ export class WebTelemetryTransportDefault implements WebTelemetryTransport {
     }
 }
 
+export class WebTelemetryTransportWithHeaders implements WebTelemetryTransport {
+    private url: string;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    send(body: string) {
+        if (navigator.sendBeacon) {
+            const blob = new Blob([body], { type: 'application/json' });
+            navigator.sendBeacon(this.url, blob);
+        } else {
+            fetch(this.url, {
+                body,
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            });
+        }
+    }
+}
+
 export class WebTelemetryTransportDebug implements WebTelemetryTransport {
     send(body: string) {
         try {
